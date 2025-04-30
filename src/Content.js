910f5f17c053e11a2d989f798d0e84df9dbba3f6
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2025-04-29 21:40:17 -0300

    feat: Created new portfolio

---
+++ src/Content.js
@@ -0,0 +1,236 @@
+// import images
+import Hero_person from "./assets/images/Hero/person.png";
+
+import next from "./assets/images/Skills/next.png";
+import nodejs from "./assets/images/Skills/node.png";
+import csharp from "./assets/images/Skills/csharp.png";
+import typescript from "./assets/images/Skills/typescript.png";
+import tailwind from "./assets/images/Skills/tailwind.png";
+import mysql from "./assets/images/Skills/mysql.png";
+import mongodb from "./assets/images/Skills/mongodb.png";
+import docker from "./assets/images/Skills/docker.png";
+import kubernetes from "./assets/images/Skills/kubernetes.png";
+/*import zbrush from "./assets/images/Skills/zbrush.png";
+import maya from "./assets/images/Skills/maya.png";
+import photoshop from "./assets/images/Skills/photoshop.png";
+*/
+
+import services_logo1 from "./assets/images/Services/logo1.png";
+import services_logo2 from "./assets/images/Services/logo2.png";
+import services_logo3 from "./assets/images/Services/logo3.png";
+
+import project1 from "./assets/images/projects/img1.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
+
+import avatar1 from "./assets/images/Testimonials/avatar1.png";
+import avatar2 from "./assets/images/Testimonials/avatar2.png";
+import avatar3 from "./assets/images/Testimonials/avatar3.png";
+import avatar4 from "./assets/images/Testimonials/avatar4.png";
+
+import Hireme_person from "./assets/images/Hireme/person.png";
+import Hireme_person2 from "./assets/images/Hireme/person2.png";
+
+// import icons from react-icons
+import { GrMail } from "react-icons/gr";
+import { MdArrowForward, MdCall } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { TbSmartHome } from "react-icons/tb";
+import { BiUser } from "react-icons/bi";
+import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+
+export const content = {
+    nav: [
+        {
+            link: "#home",
+            icon: TbSmartHome,
+        },
+        {
+            link: "#skills",
+            icon: BiUser,
+        },
+        {
+            link: "#services",
+            icon: RiServiceLine,
+        },
+        {
+            link: "#projects",
+            icon: RiProjectorLine,
+        },
+        {
+            link: "#contact",
+            icon: MdOutlinePermContactCalendar,
+        },
+    ],
+    hero: {
+        title: "Fullstack Web Developer",
+        firstName: "ROGER",
+        LastName: "ALBUQUERQUE",
+        btnText: "Hire Me",
+        image: Hero_person,
+        hero_content: [
+            {
+                count: "4+",
+                text: "Years learning about Fullstack Web development",
+            },
+        ],
+    },
+    skills: {
+        title: "Skills",
+        subtitle: "MY TOP SKILLS",
+        skills_content: [
+            {
+                name: "NextJs",
+                logo: next,
+            },
+            {
+                name: "Nodejs",
+                logo: nodejs,
+            },
+            {
+                name: "C#",
+                logo: csharp,
+            },
+            {
+                name: "Typescript",
+                logo: typescript,
+            },
+            {
+                name: "Tailwind",
+                logo: tailwind,
+            },
+            {
+                name: "MySQL",
+                logo: mysql
+            },
+            {
+                name: "MongoDB",
+                logo: mongodb,
+            },
+            {
+                name: "Docker",
+                logo: docker,
+            },
+
+            {
+                name: "Kubernetes",
+                logo: kubernetes,
+            },
+            /*
+            {
+                name: "Photoshop",
+                logo: photoshop,
+            },
+            {
+                name: "Zbrush",
+                para: "3D High poly modeling",
+                logo: zbrush,
+            },
+            {
+                name: "Autodesk Maya",
+                para: "3D modeling & Animation",
+                logo: maya,
+            },
+            */
+        ],
+    },
+    services: {
+        title: "Services",
+        subtitle: "WHAT I OFFER",
+        service_content: [
+            {
+                title: "Frontend",
+                para: "I can develop the interface and visual experience of websites and applications, transforming designs into interactive pages.",
+                logo: services_logo1,
+            },
+            {
+                title: "Backend",
+                para: "I can create and maintain the logic behind systems and applications, taking care of servers, databases and business rules.",
+                logo: services_logo2,
+            },
+            {
+                title: "Mobile",
+                para: "I can create modern and efficient applications for smartphones, delivering intuitive and optimized experiences right in the palm of your hand.",
+                logo: services_logo3,
+            },
+        ],
+    },
+    Projects: {
+        title: "Projects",
+        subtitle: "MY CREATION",
+        image: person_project,
+        project_content: [
+            {
+                title: "Gym Website",
+                image: project1,
+            },
+            {
+                title: "Social Media web",
+                image: project2,
+            },
+            {
+                title: "Creative Website",
+                image: project3,
+            },
+        ],
+    },
+    Testimonials: {
+        title: "Testimonials",
+        subtitle: "MY CLIENT REVIEWS",
+        testimonials_content: [
+            {
+                review:
+                "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+                img: avatar1,
+                name: "JOHN DOE",
+            },
+            {
+                review:
+                "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+                img: avatar2,
+                name: "Tom Alex",
+            },
+            {
+                review:
+                "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+                img: avatar3,
+                name: "Johnny",
+            },
+            {
+                review:
+                "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+                img: avatar4,
+                name: "ROBBIN",
+            },
+        ],
+    },
+    Hireme: {
+        title: "Hire Me",
+        subtitle: "FOR YOUR PROJECTS",
+        image1: Hireme_person,
+        image2: Hireme_person2,
+        para: "I like to transform challenges into solutions that really work. If you need someone to take care of your project with attention, clarity and responsibility, I'm here for that. Let's build something incredible together?",
+        btnText: "Hire Me",
+    },
+    Contact: {
+        title: "Contact Me",
+        subtitle: "GET IN TOUCH",
+        social_media: [
+            {
+                text: "rogeralbuquerque284@gmail.com",
+                icon: GrMail,
+                link: "mailto:rogeralbuquerque284@gmail.com",
+            },
+            {
+                text: "roger_dev4",
+                icon: BsInstagram,
+                link: "https://www.instagram.com/roger_dev4/",
+            },
+        ],
+    },
+    Footer: {
+        text: "All © Copy Right Reserved 2022",
+    },
+};
